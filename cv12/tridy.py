213194d20956d52eb387beae935cd2d7f1d6ccd2
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2022-01-03 14:37:26 +0100

    tabs -> spaces.

--- cv12/tridy.py
+++ cv12/tridy.py
@@ -1,27 +1,30 @@
 from turtle import dot,goto,penup, pendown, exitonclick
 class Rectangle:
-	def __init__(self,ll,ur):
-		pass
+    def __init__(self,ll,ur):
+        pass
 
-	def draw():
-		pass
+    def draw(self):
+        pass
 
 class Point:
-	def __init__(self,x=0,y=0) -> None:
-		self.x = x
-		self.y = y
-	
-	def draw(self) -> None:
-		penup()
-		goto(self.x, self.y)
-		pendown()
-		dot(10)
-	
-	def __str__(self) -> str:
-		return f"Point({self.x}, {self.y})"
+    def __init__(self,x=0,y=0) -> None:
+        self.x = x
+        self.y = y
+    
+    def draw(self) -> None:
+        penup()
+        goto(self.x, self.y)
+        pendown()
+        dot(10)
+    
+    def __str__(self) -> str:
+        return f"Point({self.x}, {self.y})"
 
 pt = Point(10,10)
 pt2 = Point()
+rect = Rectangle(pt2, pt)
+print(rect)
+rect.draw()
 print(f"pt1: {pt}, pt2: {pt2}")
 pt.draw()
 pt2.draw()
